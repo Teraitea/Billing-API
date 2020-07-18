ab3482a32347245f9628d4c2c4ebf3c810adc125
Tarihaa Teraitea <teraitea.tarihaa@gmail.com>
2020-07-18 06:22:29 -1000

    Adding database table users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('last_name');
+            $table->string('first_name');
+            $table->string('login');
+            $table->string('hierarchy_level');
+            $table->integer('distrib_id');
+            $table->date('birthday_date');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
